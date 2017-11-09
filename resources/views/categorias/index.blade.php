c929a8dd475145f6f6df0b80dc98987f86f743e7
Washington Soares <wsoaresfilho@hotmail.com.br>
2017-11-09 20:07:09 -0200

    Organizando o código das categorias

--- resources/views/categorias/index.blade.php
+++ resources/views/categorias/index.blade.php
@@ -1,51 +1,46 @@
 {{ config('app.name', 'Laravel') }}
-
-
 @extends('layouts.app')
-
 @section('content')
 
 @if ( Session::get('message'))
- <div class="alert alert-success"> {{ Session::get('message') }} </div>
+    <div class="alert alert-success"> {{ Session::get('message') }} </div>
 @endif
 
 
 <div class="row">
-                    <div class="col-md-12">
-                        <div class="panel">
-                            <div class="panel-heading">CATEGORIAS</div>
-                            <div class="table-responsive">
-                                <table class="table table-hover manage-u-table">
-                                    <thead>
-                                        <tr>
-                                            <th width="70" class="text-center">#</th>
-                                            <th>NOME</th>
-                                            <th>DESCRIÇÃO</th>
-                                            <th width="300">DELETAR</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
- 
-    @foreach($categorias as $categoria)
-    <tr>
+    <div class="col-md-12">
+        <div class="panel">
+            <div class="panel-heading">CATEGORIAS</div>
+            <div class="table-responsive">
+                <table class="table table-hover manage-u-table">
+                    <thead>
+                        <tr>
+                            <th width="70" class="text-center">#</th>
+                            <th>NOME</th>
+                            <th>DESCRIÇÃO</th>
+                            <th width="300">DELETAR</th>
+                        </tr>
+                    </thead>
+                    <tbody>
 
-        <td>{{$categoria->id}}</td>
-        <td><a href="/categorias/edit/{{ $categoria->id }}">{{ $categoria->nome }}</a></td>
-        <td>{{ $categoria -> descricao }}</td>
-            <td><a href="/categorias/delete/{{ $categoria->id }}">Remover</a></td>
+                    @foreach($categorias as $categoria)
+                        <tr>
 
-        <hr>
-    </tr>
-    @endforeach
+                            <td>{{$categoria->id}}</td>
+                            <td><a href="/categorias/edit/{{ $categoria->id }}">{{ $categoria->nome }}</a></td>
+                            <td>{{ $categoria -> descricao }}</td>
+                                <td><a href="/categorias/delete/{{ $categoria->id }}">Remover</a></td>
 
-</tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </div>    
+                            <hr>
+                        </tr>
+                    @endforeach
 
-    <a href="/categorias/add">Adicionar</a>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+<a href="/categorias/add">Adicionar</a>
 @endsection
-
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
                     <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
                         <h4 class="page-title">Home</h4> </div>
                     <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
-                        <button class="right-side-toggle waves-effect waves-light btn-info btn-circle pull-right m-l-20"><i class="ti-settings text-white"></i></button>                        
+
                         <ol class="breadcrumb">
                             <li><a href="#">Home</a></li>
                             <li class="active">Home</li>
